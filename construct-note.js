7cee736ba71f29d0650047505d372d5668d868f2
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-06-12 21:05:54 -0700

    adding construct-note

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+// letterCount helper function
+const letterCount = (str) => {
+  let count = new Map();
+
+  for (let val of str) {
+    let valCount = count.get(val) || 0;
+    count.set(val, valCount + 1);
+  }
+
+  return count;
+};
+
+function constructNote(message, letter) {
+  // count letter in message and letter
+  let messageMap = letterCount(message);
+  let letterMap = letterCount(letter);
+
+  // Loop over each letter in the message.
+  for (let letter of messageMap.keys()) {
+    // Check if letter exists in letterMap and if the count is enough.
+    if (
+      !letterMap.has(letter) ||
+      messageMap.get(letter) > letterMap.get(letter)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
